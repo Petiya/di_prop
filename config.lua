b84d5711026c9d0d434deb54e8329da71b9eb145
Petiya <h.lakshitha@gmail.com>
2025-07-26 09:50:28 +0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,21 @@
+
+Config = {}
+
+-- Define crafting locations
+Config.CraftingLocations = {
+    {
+        coords = vector3(-1144.57, 4937.19, 222.27), -- Example coordinates
+        prop = 'prop_tablesaw_01',               -- Example prop to be placed
+        heading = 160.16                          -- Example heading
+    },
+    {
+        coords = vector3(-1143.36, 4936.55, 222.27), -- Example coordinates
+        prop = 'prop_vertdrill_01',            -- Another example prop
+        heading = 139.1                        -- Example heading
+    },
+    {
+        coords = vector3(-1149.18, 4938.70, 222.27), -- Example coordinates
+        prop = 'p_gcase_s',            -- Another example prop
+        heading = 139.1                        -- Example heading
+    }
+}
